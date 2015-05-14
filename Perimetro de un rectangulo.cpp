3c51487be96f414f8885e7cbf95e3679ec9c51ab
nburza <nicoburza_96@hotmail.com>
2015-05-14 17:47:40 -0300

    FUNCIONES

---
+++ Perimetro de un rectangulo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+double GetNumeroPositivo();
+double GetPerimetro(double, double);
+
+int main (){
+	cout<<"Este programa calculara el perimetro de un rectangulo\n\n";
+	cout<<"Ingrese la medida de la altura del rectangulo: ";
+	double h=GetNumeroPositivo();
+	cout<<"Ahora ingrese la medida de la base: ";
+	double b=GetNumeroPositivo();
+	cout<<"El perimetro del rectangulo es de: "<<GetPerimetro(h,b);
+}
+
+double GetNumeroPositivo(){
+	double numero;
+	do{
+		cin>>numero;
+	}while(numero<=0);
+}
+
+double GetPerimetro(h, b){
+	2*(b+h);
+}
